e476dd21b563c1ef856727a574a3e04e9a656820
garySZA <garysoliz07@gmai.com>
2022-12-29 09:46:02 -0400

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryp = require("bcryptjs");
 
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt.js");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async ( req, res = response ) => {
     const { correo, password } = req.body;
@@ -49,6 +50,56 @@ const login = async ( req, res = response ) => {
     }
 }
 
+const googleSignIn = async ( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try{
+        const { nombre, img, correo } = await googleVerify( id_token );
+        
+        let usuario = await Usuario.findOne({ correo });
+        
+        if( !usuario ){
+            //Crear un usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            }
+            
+            usuario = new Usuario( data );
+            await usuario.save();
+            
+        }
+
+        //Si el usuario en DB
+        if( !usuario.estado ){
+            return res.status(401).json({
+                error: "Hable con el administrador, usuario bloqueado"
+            });
+
+        }
+
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch(e){
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        });
+    }
+    
+}
+
 module.exports = {
     login,
+    googleSignIn
 }
